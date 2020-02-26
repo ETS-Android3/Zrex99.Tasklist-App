e3ee60045da1e958646fb9b56fb69fd21e6aaee1
Zakarie Ortiz <ZOrtiz42@outlook.com>
2020-02-26 18:03:50 -0500

    ADD
    -Added layout files for the main activity and tasklist fragment.
    
    -Added rounded shape drawable for achieving the custom look.

--- ChecklistAppDevelopment/app/src/main/java/com/zoportfolio/checklistproject/MainActivity.java
+++ ChecklistAppDevelopment/app/src/main/java/com/zoportfolio/checklistproject/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Assign the FAB its on click listener, load the tasklist fragment, and load the date for the textview.
     }
 }
